660df5f9f9dc60d0fb679fef6f4435d80f103e9c
weidong1 <462927532@qq.com>
2022-11-25 19:44:17 +0800

    tmp1

--- demo.py
+++ demo.py
@@ -1,1 +1,25 @@
-this is a test.
+''' 
+test torch-gpu env
+'''
+
+import torch
+from torch.backends import cudnn 
+
+a = torch.cuda.is_available()
+print(a)
+ngpu= 1
+device = torch.device("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else "cpu")
+print(device)
+print(torch.cuda.get_device_name(0))
+print(torch.rand(3,3).cuda())
+
+
+print("判断是否安装了cuDNN")
+print(cudnn.is_available())  #返回True则说明已经安装了cuDNN
+
+''' 测试tf-gpu
+import tensorflow as tf
+from tensorflow.python.client import device_lib
+print(device_lib.list_local_devices())
+print(tf.test.is_gpu_available())
+'''
