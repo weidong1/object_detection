80a532e807e8e69c54c66ef496c2c7cc636f747f
weidong1 <462927532@qq.com>
2022-11-28 14:13:49 +0800

    add 3

--- yolov5/train_dw.sh
+++ yolov5/train_dw.sh
@@ -1,2 +1,2 @@
 # Train YOLOv5s on COCO128 for 3 epochs
-python train_dw.py --img 640 --batch 16 --epochs 3 --data coco128.yaml --weights ./checkpoints/yolov5s.pt
+python train_dw.py --img 640 --batch 16 --epochs 3 --data ./data/coco128.yaml --weights ./checkpoints/yolov5s.pt
