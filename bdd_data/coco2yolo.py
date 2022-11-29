06180ce905ded29cf1d6a05e2f253e1f4607c26d
weidong1 <462927532@qq.com>
2022-11-29 16:07:32 +0800

    add 9

---
+++ bdd_data/coco2yolo.py
@@ -0,0 +1,66 @@
+#-*-coding:utf-8-*-
+# 把coco格式转换成yolo格式
+
+# python3 coco2yolo.py --datasets COCO --img_path /home/iav/code/bdd100k_yolov5/bdd100k_images/bdd100k/images/100k/train --label labels/bdd100k_labels_images_det_coco_val.json --convert_output_path train_labels/ --img_type ".jpg" --manipast_path ./ --cls_list_file bdd100k.names
+
+import os
+from xml.etree.ElementTree import dump
+import json
+import pprint
+
+import argparse
+
+from Format import COCO,YOLO
+
+def main(config):
+
+    if config["datasets"] == "COCO":
+        coco = COCO()
+        yolo = YOLO(os.path.abspath(config["cls_list"]))
+
+        flag, data = coco.parse(config["label"])
+
+        if flag == True:
+            flag, data = yolo.generate(data)
+
+            if flag == True:
+                flag, data = yolo.save(data, config["output_path"], config["img_path"],
+                                        config["img_type"], config["manipast_path"])
+
+                if flag == False:
+                    print("Saving Result : {}, msg : {}".format(flag, data))
+
+            else:
+                print("YOLO Generating Result : {}, msg : {}".format(flag, data))
+
+        else:
+            print("COCO Parsing Result : {}, msg : {}".format(flag, data))
+
+    else:
+        print("Unkwon Datasets")
+
+if __name__ == '__main__':
+
+    config ={
+        "datasets": "COCO",
+        "img_path": "../../bdd_data_archive/bdd100k/bdd100k/images/100k/train",
+        "label": "../../bdd_data_archive/parsed/bdd100k_labels_images_det_coco_train.json",
+        "img_type": ".jpg",
+        "manipast_path": "./",
+        "output_path": "../../bdd_data_archive/parsed/",
+        "cls_list": "./bdd100k.names",
+    }
+
+    "../../bdd_data_archive/bdd100k_labels_release/bdd100k/labels/",
+
+    # config ={
+    #     "datasets": "COCO",
+    #     "img_path": "../../bdd_data_archive/bdd100k/bdd100k/images/100k/val",
+    #     "label": "../../bdd_data_archive/parsed/bdd100k_labels_images_det_coco_val.json",
+    #     "img_type": ".jpg",
+    #     "manipast_path": "./",
+    #     "output_path": "../../bdd_data_archive/parsed/",
+    #     "cls_list": "./bdd100k.names",
+    # }
+
+    main(config)
